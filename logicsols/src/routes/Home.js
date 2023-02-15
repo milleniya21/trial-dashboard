e0c01b614b66f7e799a106c99687d4789e6179cc
milleniya21 <milleniyasajy21@gmail.com>
2023-02-15 09:07:02 +0530

    navbar-created

---
+++ logicsols/src/routes/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className='home'> 
+            <p>This is your Dashboard </p>
+            <h1>Hello Thomas</h1>
+        </div>
+    );
+}
